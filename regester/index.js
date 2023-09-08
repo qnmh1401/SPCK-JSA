452ef7f7adf87c24f1e0d5b1d4911291f961bc7e
D0ubleCute <trandotrungkien@gmail.com>
2023-09-08 20:20:19 +0700

    signUp signIn

--- regester/index.js
+++ regester/index.js
@@ -9,38 +9,48 @@ var showPass = document.getElementById("checkbox");
 var labelShow = document.getElementById("onpass");
 labelShow.innerText = "Show password";
 passWord.type = "password";
-showPass.addEventListener("click", showPassWord);
-
-
 
 checkbox.addEventListener("change", function () {
     if (this.checked) {
-        passWord.type = "text";
-        labelShow.innerText = "Hide password";
-      } else {
-        passWord.type = "password";
-        labelShow.innerText = "Show password";
-      }
-    });
+      passWord.type = "text";
+      labelShow.innerText = "Hide password";
+    } else {
+      passWord.type = "password";
+      labelShow.innerText = "Show password";
+    }
+  });
+  
 
 function handleClick (e){
     var invalidEmail = document.getElementById('invalidemail')
     var invalidPass = document.getElementById('invalidpass')
 
-    if(email.value == ""){
-        invalidEmail.innerText = "Email is empty"
-    }else if(!filter.test(email.value)){
-        invalidEmail.innerText = "Wrong email"
-    }else{
-        invalidEmail.innerText =""
+    if (email.value == "" || email == null) {
+      invalidEmail.innerText = "Email is empty";
+    } else if (!filter.test(email.value)) {
+      invalidEmail.innerText = "Wrong email";
+    } else {
+      invalidEmail.innerText = "";
+    }
+  
+    if (passWord.value == "" || passWord == null) {
+      invalidPass.innerText = "Password is empty";
+    } else if (passWord.value.length < 6) {
+      invalidPass.innerText = "Passwords must be at least 6 characters";
+    } else {
+      invalidPass.innerText = "";
     }
 
-    if(passWord.value == ""){
-        invalidPass.innerText = "Password is empty"
-    }else if((passWord.value).length < 6 ){
-        invalidPass.innerText = "Passwords must be at least 6 characters"
-    }else{
-        invalidPass.innerText =""
+    if (
+      (email.value != "" && email != null) &&
+      filter.test(email.value) &&
+      (passWord.value != "" &&
+      passWord.value.length >= 6 &&
+      passWord != null)
+    ) {
+      window.location = "../signin/index.html";
+    } else {
+      e.preventDefault();
     }
 
     // for (let account of formData) {
@@ -56,21 +66,27 @@ function handleClick (e){
     // }
     // }
 }
-   console.log(formData)
+
 function signIn(e) {
-    let email = document.getElementById("email").value,
-      pwd = document.getElementById("pwd").value;
-    var formData = JSON.parse(localStorage.getItem("formData")) || [];
-    let exist =
-      formData.length &&
-      JSON.parse(localStorage.getItem("formData")).some(
-        (data) =>
-          data.email.toLowerCase() == email.toLowerCase() && data.pwd == pwd
-      );
-    if (!exist) {
-      alert("Incorrect login credentials");
-    } else {
-      location.href = "../HomePage.html";
-    }
+  let email = document.getElementById("inputemail").value,
+    pwd = document.getElementById("inputpass").value;
+
+  let formData = JSON.parse(localStorage.getItem("formData")) || [];
+
+  let exist =
+    formData.length &&
+    JSON.parse(localStorage.getItem("formData")).some(
+      (data) => data.email.toLowerCase() == email.toLowerCase()
+    );
+
+  if (!exist) {
+    alert("Account Has Incorrect credentials");
     e.preventDefault();
+  } else {
+    location.replace="https://www.w3schools.com/jsref/met_loc_replace.asp"
   }
+}
+
+console.log(localStorage.getItem("formData"));
+
+  document.querySelector("form").addEventListener("submit", handleClick, signIn);
--- regester/signup.js
+++ regester/signup.js
@@ -25,13 +25,13 @@ checkbox.addEventListener("change", function () {
     var invalidName = document.getElementById("invalidname");
     var invalidEmail = document.getElementById("invalidemail");
     var invalidPass = document.getElementById("invalidpass");
-    if (yourName.value == "") {
+    if (yourName.value == "" || yourName == null) {
         invalidName.innerText = "Name is empty";
       } else {
         invalidName.innerText = "";
       }
     
-      if (email.value == "") {
+      if (email.value == "" || email == null) {
         invalidEmail.innerText = "Email is empty";
       } else if (!filter.test(email.value)) {
         invalidEmail.innerText = "Wrong email";
@@ -39,7 +39,7 @@ checkbox.addEventListener("change", function () {
         invalidEmail.innerText = "";
       }
     
-      if (passWord.value == "") {
+      if (passWord.value == "" || passWord == null) {
         invalidPass.innerText = "Password is empty";
       } else if (passWord.value.length < 6) {
         invalidPass.innerText = "Passwords must be at least 6 characters";
@@ -48,13 +48,16 @@ checkbox.addEventListener("change", function () {
       }
     
       if (
-        !yourName.value == "" &&
-        !email.value == "" &&
+        (yourName.value != "" && yourName != null) &&
+        (email.value != "" && email != null) &&
         filter.test(email.value) &&
-        !passWord.value == "" &&
-        !passWord.value.length < 6
+        (passWord.value != "" &&
+        passWord.value.length >= 6 &&
+        passWord != null)
       ) {
         window.location = "../signin/index.html";
+      } else {
+        e.preventDefault();
       }
     
       //   const accounts = [
@@ -66,8 +69,11 @@ checkbox.addEventListener("change", function () {
     
       //   localStorage.setItem("accounts", JSON.stringify(accounts));
     }
+
+    console.log(localStorage.getItem("formData"));
+
     
-    document.querySelector("form").addEventListener("submit", signUp);
+    document.querySelector("form").addEventListener("submit", handleClick, signUp);
     
     function signUp(e) {
       let name = document.getElementById("inputinfor").value,
@@ -87,9 +93,9 @@ checkbox.addEventListener("change", function () {
         localStorage.setItem("formData", JSON.stringify(formData));
         document.querySelector("form").reset();
         alert("Account Created Successfully");
+        window.location.replace = "../HomePage.html"
       } else {
         alert("Ooopppssss... Duplicate found!!!\nYou have already signed up");
-        location.href = "../signin/index.html"
       }
       e.preventDefault();
     }
